aab2c00e86b3d1c1690475f5301ec6a460f416fb
MikeZande <mikevdzande@gmail.com>
2026-01-10 16:36:09 +0100

    Made a start on frontend. There is a button which requests questions from backend.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,28 +3,52 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const API_URL = "http://localhost:8080/";
-  const [welcomeMsg, setWelcomeMsg] = useState("Default value");
+  const [questions, setQuestions] = useState(null);
 
-  useEffect(() => {
-    const fetchMessage = async () => {
-      try {
-        const res = await fetch(API_URL);
-        if (!res.ok) {
-          throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        const data = await res.text();
-        setWelcomeMsg(data);
-      } catch (error) {
-        console.error("Failed to fetch welcome message:", error);
+  async function getQuestions(
+    amount?: number,
+    category?: number,
+    difficulty?: string,
+    type?: string
+  ) {
+    const params = new URLSearchParams();
+    if (amount) params.append("amount", amount.toString());
+    if (category) params.append("category", category.toString());
+    if (difficulty) params.append("difficulty", difficulty);
+    if (type) params.append("type", type);
+
+    const uri = API_URL + "questions?" + params;
+    console.log(uri);
+
+    try {
+      const response = await fetch(uri);
+      const result = await response.json();
+
+      if (response.status === 429) {
+        console.log(result);
+        console.warn("Rate limit exceeded. Please wait 5 seconds before requesting.");
+        return result;
       }
-    };
 
-    fetchMessage();
-  }, []);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      console.log(result);
+      return result;
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
 
   return (
-    <div>
-      Hey + {welcomeMsg} 
+    <div className="p-8 min-h-screen bg-slate-950">
+      <button 
+        className="px-4 py-2 bg-blue-500 rounded hover:bg-blue-600"
+        onClick={() => getQuestions()}
+      >
+        Request Questions
+      </button>
     </div>
   );
 }
