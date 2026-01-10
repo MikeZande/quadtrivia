b0f011212c9b5311d65d1da034cdd24e2820036b
MikeZande <mikevdzande@gmail.com>
2026-01-10 01:42:23 +0100

    Small bug fixes.

--- backend/src/main/java/com/mike/quadtrivia/models/TokenResponse.java
+++ backend/src/main/java/com/mike/quadtrivia/models/TokenResponse.java
@@ -1,7 +1,6 @@
 package com.mike.quadtrivia.models;
 
 import com.mike.quadtrivia.enums.ResponseCode;
-import tools.jackson.databind.annotation.JsonDeserialize;
 
 public record TokenResponse(
         ResponseCode response_code,
