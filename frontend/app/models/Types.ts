4d52d3ebbf0536b1d2051dd90e211aca299807f1
MikeZande <mikevdzande@gmail.com>
2026-01-11 12:18:29 +0100

    Added parameter selection to ui.

--- frontend/app/models/Types.ts
+++ frontend/app/models/Types.ts
@@ -1,3 +1,9 @@
-export type Difficulty = "easy" | "medium" | "hard";
-export type QuestionType = "boolean" | "multiple";
+// Arrays iterated over by select boxes.
+export const DIFFICULTIES = ["easy", "medium", "hard"] as const;
+export const QUESTION_TYPES = ["multiple", "boolean"] as const;
+export const CATEGORIES = ["10", "11"] as const; // TODO: get the proper categories somehow.
+
+export type Category = typeof CATEGORIES[number];
+export type Difficulty = typeof DIFFICULTIES[number];
+export type QuestionType = typeof QUESTION_TYPES[number];
 export type ResponseCode = "SUCCESS" | "NO_RESULTS" | "INVALID_PARAMETER" | "RATE_LIMIT_EXCEEDED";
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,27 @@
 "use client"
 import { useState } from "react";
 import { Question } from "./models/Question";
-import { Difficulty, QuestionType } from "./models/Types";
+import { Difficulty, QuestionType, ResponseCode, Category, DIFFICULTIES, QUESTION_TYPES, CATEGORIES } from "./models/Types";
 import { QuestionResponse } from "./models/QuestionResponse";
 
 export default function Home() {
   const API_URL = "http://localhost:8080/";
   const [questions, setQuestions] = useState<Question[]>([]);;
+  const [responseCode, setResponseCode] = useState<ResponseCode | null>(null);
 
-  async function getQuestionResponse(
-    amount?: number,
-    category?: number,
-    difficulty?: Difficulty,
-    type?: QuestionType
-  ) : Promise<QuestionResponse> {
+  // Query params for getQuestions.
+  const [questionAmount, setQuestionAmount] = useState<number>(5);
+  const [category, setCategory] = useState<Category | undefined>(undefined);
+  const [difficulty, setDifficulty] = useState<Difficulty | undefined>(undefined);
+  const [questionType, setQuestionType] = useState<QuestionType | undefined>(undefined);
+
+  async function getQuestionResponse() : Promise<QuestionResponse> {
 
     const params = new URLSearchParams();
-    if (amount !== undefined) params.append("amount", amount.toString());
-    if (category !== undefined) params.append("category", category.toString());
-    if (difficulty) params.append("difficulty", difficulty);
-    if (type) params.append("type", type);
+    params.append("amount", questionAmount.toString());
+    if (category !== undefined) params.append("category", category);
+    if (difficulty !== undefined) params.append("difficulty", difficulty);
+    if (questionType !== undefined) params.append("type", questionType);
 
     const uri = API_URL + "questions?" + params;
 
@@ -38,15 +40,16 @@ export default function Home() {
       }
 
       return result;
-    } catch (error: any) {
+    } catch (error) {
       throw new Error("Something went wrong");
     }
   }
 
   const handleClick = async () => {
     try {
-      const response: QuestionResponse = await getQuestionResponse(10);
+      const response: QuestionResponse = await getQuestionResponse();
       setQuestions(response.questions);
+      setResponseCode(response.responseCode);
       console.log(response);
       console.log(questions);
     } catch (err) {
@@ -54,9 +57,16 @@ export default function Home() {
     }
   };
 
+  function handleNrInput(e: React.ChangeEvent<HTMLInputElement>) {
+    let value = Number(e.target.value);
+    if (value < 1) value = 1;
+    if (value > 50) value = 50;
+    setQuestionAmount(value);
+  }
+
   return (
     <div className="p-8 min-h-screen bg-slate-950">
-      <div className="h-[70vh] w-full overflow-y-auto">
+      <div className="h-[65vh] w-full overflow-y-auto mb-8">
         <table className="w-full min-h-full border-collapse-seperate">
           <thead className="bg-teal-600 sticky top-0 z-10 border">
             <tr>
@@ -87,7 +97,7 @@ export default function Home() {
                               type="radio"
                               name={question.id}
                               value={answer}
-                              className="accent-teal-600"
+                              className="accent-teal-600 hover:cursor-pointer"
                             />
                             <span>{answer}</span>
                           </label>
@@ -101,12 +111,132 @@ export default function Home() {
         </table>
       </div>
 
-      <button 
-        className="px-6 py-4 bg-teal-600 rounded-full hover:bg-teal-500 hover:cursor-pointer mt-8"
-        onClick={handleClick}
-      >
-        Generate New Questions
-      </button>
+      <div className="flex flex-col w-fit gap-2">
+        {/* Amount of questions */}
+        <div className="flex gap-4">
+          <span className="w-40">
+            Number of Questions:
+          </span>
+
+          <input
+            className="
+              bg-slate-900 
+              border-3 border-gray-300
+              rounded-md
+              w-60
+              hover:cursor-pointer"
+            type="number"
+            min={1}
+            max={50}
+            value={questionAmount}
+            onChange={(e) => handleNrInput(e)}
+          />
+        </div>
+
+        {/* Category */}
+        <div className="flex gap-4">
+          <span className="w-40">
+            Category:
+          </span>
+
+          <select 
+            className="
+              bg-slate-900 
+              border-3 border-gray-300
+              rounded-md
+              w-60
+              hover:cursor-pointer"
+            onChange={(e) => 
+              setCategory(
+                e.target.value === "" ? undefined : (e.target.value as Category)
+              )
+            }
+          >
+            <option value=""> All </option>
+            {CATEGORIES.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Difficulty */}
+        <div className="flex gap-4">
+          <span className="w-40">
+            Difficulty:
+          </span>
+
+          <select 
+            className="
+              bg-slate-900 
+              border-3 border-gray-300
+              rounded-md
+              w-60
+              hover:cursor-pointer"
+            onChange={(e) => 
+              setDifficulty(
+                e.target.value === "" ? undefined : (e.target.value as Difficulty)
+              )
+            }
+          >
+            <option value=""> All </option>
+            {DIFFICULTIES.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Question Type */}
+        <div className="flex gap-4">
+          <span className="w-40">
+            Question Type:
+          </span>
+
+          <select 
+            className="
+              bg-slate-900 
+              border-3 border-gray-300
+              rounded-md
+              w-60
+              hover:cursor-pointer"
+            onChange={(e) => 
+              setQuestionType(
+                e.target.value === "" ? undefined : (e.target.value as QuestionType)
+              )
+            }
+          >
+            <option value=""> All </option>
+            {QUESTION_TYPES.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Generate questions button */}
+        <button 
+          className="px-6 py-4 bg-teal-600 rounded-full hover:bg-teal-500 hover:cursor-pointer mt-2"
+          onClick={handleClick}
+        >
+          Generate New Questions
+        </button>
+
+        {responseCode && (
+          <p 
+            className={`mt-3 text-m text-center text-wrap ${
+              responseCode === "SUCCESS" 
+                ? "text-green-500"
+                : "text-red-500"
+            }`}
+          >
+            {responseCode}
+          </p>
+        )}
+      </div>
     </div>
   );
 }
